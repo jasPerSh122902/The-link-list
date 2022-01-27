40f1f7cc2ac77fb11ff25433b108e0e2a58e9c4c
jasPerSh122902 <88592760+jasPerSh122902@users.noreply.github.com>
2022-01-27 16:52:36 -0600

    this is the update that is at rods house but ...
    added commits were i think they need to be and they will explain. But now i need to make sure that alll of the info is corrent and not erors

--- Link-list/Link-list/List.h
+++ Link-list/Link-list/List.h
@@ -21,7 +21,8 @@ public:
 	 bool contain(const T object)const;
 	void pushFront(const T& value);
 	void pushBack(const T& value);
-	bool insert(const T& value, int index);
+	bool insert(const T& value, int
+		index);
 	bool remove(const T& value);
 	 void print()const;
 	void initialize();
@@ -58,9 +59,9 @@ inline List<T>::~List()
 template<typename T>
 inline void List<T>::destroy()
 {
-	for (int i = 0; i < m_nodeCount; i++)
+	for (int i = m_first; i <= m_last; i++)
 	{
-		delete Node;
+		delete m_nodeCount;
 	}
 }
 
@@ -93,24 +94,76 @@ inline bool List<T>::contain(const T object) const
 template<typename T>
 inline void List<T>::pushFront(const T& value)
 {
+	//T* next;
+	//T* previous;
+	//can i add pointer here
 	value = m_first
 }
 
 template<typename T>
 inline void List<T>::pushBack(const T& value)
 {
+	//T* next;
+	//T* previous;
+	//can i add pointer here
+	for (value = index; value)
+
 }
 
 template<typename T>
 inline bool List<T>::insert(const T& value, int index)
 {
+	index = m_nodeCount;
+	//can i add pointer here 
+
+	//is it a while loop or a for loop
+	for (int i = value; i <= index; i++)
+	{
+		//also this needs some work
+		pushBack(value[i]);
+		pushFront(value[i]);
+	}
 	return false;
+
 }
 
 template<typename T>
 inline bool List<T>::remove(const T& value)
 {
-	return false;
+	//Check to see if the actor was null
+	if (!value || getLength() <= 0)
+		return false;
+
+	bool actorRemoved = false;
+	//Create a new array with a size one less than our old array
+	T* newArray = new T[getLength() - 1];
+	//Create variable to access tempArray index
+	int j = 0;
+	//Copy values from the old array to the new array
+	for (int i = 0; i < getLength(); i++)
+	{
+		//if the values are the same or the actor has been removed...
+		if (value != m_nodeCount[i] || actorRemoved)
+		{
+			newArray[j] = m_nodeCount[i];//print that actor and...
+			j++;//increament j
+		}
+		else
+			actorRemoved = true;
+	}
+	//Set the old array to the new array
+	if (actorRemoved)//if actor removed == true
+	{
+		//I dont havbe a arrya that can be used that is m_items
+		//need to make a m_items or somthing taht is like it
+		//delet pushFront();
+		//delete pushBack();
+		delete[] m_items;//delets the items array 
+		m_items = newArray;//set the items to the new array
+		m_nodeCount--;//decrement length
+	}
+	//Return whether or not the removal was successful
+	return actorRemoved;
 }
 
 //change this but yes there are errors
@@ -124,6 +177,13 @@ inline void List<T>::print() const
 template<typename T>
 inline void List<T>::initialize()
 {
+	//question here about how to set the m_first to the node coutner 
+	m_first == m_nodeCount[1];//set the m_fiest to the node counter at the index of 1
+	for (m_nodeCount = 1; m_nodeCount == getLength(); m_nodeCount++)
+	{
+		//questioin is this corrent and if yes ask if there is a better way of doing this...
+		m_last == m_nodeCount;//set the m_last to the last node counter index 
+	}
 }
 
 //there is errors
@@ -138,6 +198,11 @@ inline bool List<T>::isEmpty() const
 template<typename T>
 inline bool List<T>::getData(Iterator<T>& iter, int index)
 {
+	for (index = 1; index < getLength(); index++)
+	{
+		iter == index;
+		return iter;
+	}
 	return false;
 }
 
@@ -164,6 +229,7 @@ inline void List<T>::sort()
 	{
 		for (b = c + 1; b < 4; b++)
 		{
+			//have to change the c and b
 			if (m_nodeCount[b] < m_nodeCount[c])
 			{
 				temp = m_nodeCount[c];
