7135b8bc68a23e32ab49a417510e62a0f7cf4ffd
jasper Thibodeaux <88592760+jasPerSh122902@users.noreply.github.com>
2022-02-10 02:56:46 -0600

    made the iterator now workds with list

--- Link-list/Link-list/Iterator.h
+++ Link-list/Link-list/Iterator.h
@@ -1,66 +1,58 @@
 #pragma once
+#include "Node.h"
 template <typename T>
-class Node;
-template <typename T>
-class Iterator 
-{
+class Iterator{
 public:
+	Iterator(); //Creates a new iterator that points at nothing
+	Iterator(Node<T>* node); //Creates a new iterator that points at the given node
 
-	Iterator();
-	Iterator(Node<T>* node);
-	Iterator<T> operator++();
-	Iterator<T> operator--();
-	const bool operator ==(const Iterator<T> & iter);
-	const bool operator !=(const Iterator<T>& iter);
-	T operator *();
-	
+	Iterator<T> operator ++(); //Goes to the current node's next node
+	Iterator<T> operator --(); //Goes to the current node's previous node
+	const bool operator ==(const Iterator<T>& iterator); //Checks to see if two nodes are equal
+	const bool operator !=(const Iterator<T>& iterator); //Checks to see if two nodes are not equal
+	T operator *(); //Dereferences an iterator to get the value of a node
+
 private:
-	Node<T>* m_current;
+	Node<T>* m_currentNode; //The node that the iterator is currently pointing at
 };
 
 template<typename T>
-inline Iterator<T>::Iterator()
-{
-	m_current = nullptr;
+inline Iterator<T>::Iterator() {
+	m_currentNode = nullptr;
 }
 
 template<typename T>
-inline Iterator<T>::Iterator(Node<T>* node)
-{
-	m_current = node;
+inline Iterator<T>::Iterator(Node<T>* node) {
+	m_currentNode = node;
 }
 
 template<typename T>
-inline Iterator<T> Iterator<T>::operator++()
-{
-	return Iterator<T>( m_current->next);
+inline Iterator<T> Iterator<T>::operator++() {
+	m_currentNode = m_currentNode->next;
+	return Iterator<T>();
 }
 
 template<typename T>
-inline Iterator<T> Iterator<T>::operator--()
-{
-	return Iterator<T>(m_current->previous);
+inline Iterator<T> Iterator<T>::operator--() {
+	m_currentNode = m_currentNode->previous;
+	return Iterator<T>();
 }
 
 template<typename T>
-inline const bool Iterator<T>::operator==(const Iterator<T>& iter)
-{
-	if (m_current == iter.m_current)
+inline const bool Iterator<T>::operator==(const Iterator<T>& iterator) {
+	if (m_currentNode == iterator.m_currentNode)
+		return true;
+	else return false;
+}
+
+template<typename T>
+inline const bool Iterator<T>::operator!=(const Iterator<T>& iterator) {
+	if (m_currentNode != iterator.m_currentNode)
 		return true;
 	return false;
 }
 
 template<typename T>
-inline const bool Iterator<T>::operator!=(const Iterator<T>& iter)
-{
-	if (m_current != iter.m_current)
-		return true;
-	return false;
-}
-
-//This is on hold for now
-template<typename T>
-inline T Iterator<T>::operator*()
-{
-	return m_current->data;
+inline T Iterator<T>::operator*() {
+	return m_currentNode->data;
 }
